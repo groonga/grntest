79712462e7989a010e871bdba8d522ab7468df3c
Kouhei Sutou <kou@clear-code.com>
2012-03-06 16:37:55 +0900

    [test] add

---
+++ test/test-executor.rb
@@ -0,0 +1,41 @@
+# Copyright (C) 2012  Kouhei Sutou <kou@clear-code.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require "stringio"
+require "groonga/tester"
+
+class TestExecutor < Test::Unit::TestCase
+  def setup
+    @groonga = StringIO.new
+    @executor = Groonga::Tester::Executor.new(@groonga)
+    @context = @executor.context
+    @script = Tempfile.new("test-executor")
+  end
+
+  private
+  def execute(command)
+    @script.print(command)
+    @script.close
+    @executor.execute(Pathname(@script.path))
+  end
+
+  class TestComment < self
+    def test_disable_logging
+      assert_predicate(@context, :logging?)
+      execute("# disable-logging")
+      assert_not_predicate(@context, :logging?)
+    end
+  end
+end
