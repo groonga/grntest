374924a84dca380a06906e005ccc88cd7e8fcad6
HAYASHI Kentaro <hayashi@clear-code.com>
2012-11-19 15:01:46 +0900

    Rename directive to on-error
    #@ignore-feature-on-error ->
    #@on-error

--- test/test-executor.rb
+++ test/test-executor.rb
@@ -53,9 +53,9 @@ class TestExecutor < Test::Unit::TestCase
     end
 
     def test_enable_ignore_feature_on_error
-      assert_not_predicate(@context, :ignore_feature?)
-      execute("\#@ignore-feature-on-error")
-      assert_predicate(@context, :ignore_feature?)
+      assert_not_equal(@context.on_error?, :omit)
+      execute("\#@on-error omit")
+      assert_equal(@context.on_error?, :omit)
     end
   end
 
