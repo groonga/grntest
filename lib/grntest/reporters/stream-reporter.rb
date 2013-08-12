07bdd677341ffd2210bbc2d4c2073e494dd54e14
Kouhei Sutou <kou@clear-code.com>
2013-08-12 12:16:07 +0900

    Follow word separator change in file name

--- lib/grntest/reporters/stream-reporter.rb
+++ lib/grntest/reporters/stream-reporter.rb
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require "grntest/reporters/base_reporter"
+require "grntest/reporters/base-reporter"
 
 module Grntest
   module Reporters
