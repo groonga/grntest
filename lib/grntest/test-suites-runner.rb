2896417794839d7e7b42162670f74c0fe5876e7b
Kouhei Sutou <kou@clear-code.com>
2013-08-12 12:34:36 +0900

    Add missing require

--- lib/grntest/test-suites-runner.rb
+++ lib/grntest/test-suites-runner.rb
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+require "thread"
+
 require "grntest/reporters"
 require "grntest/worker"
 require "grntest/base-result"
