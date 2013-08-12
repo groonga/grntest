8c894e7e4f2e862e5401fdd9add1b35bd0a6b75c
Kouhei Sutou <kou@clear-code.com>
2013-08-12 12:47:55 +0900

    Remove needless require

--- test/executors/test-standard-io-executor.rb
+++ test/executors/test-standard-io-executor.rb
@@ -14,7 +14,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 require "stringio"
-require "cgi/util"
 require "tempfile"
 require "grntest/executors/standard-io-executor"
 
