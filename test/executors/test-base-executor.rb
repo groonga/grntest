5a73abe035c43ae923ecb42dffd6edddaa12b020
Kouhei Sutou <kou@clear-code.com>
2014-09-28 11:24:59 +0900

    test: add more description to test case name

--- test/executors/test-base-executor.rb
+++ test/executors/test-base-executor.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2013  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2013-2014  Kouhei Sutou <kou@clear-code.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@ class TestBaseExecutor < Test::Unit::TestCase
     @context = @executor.context
   end
 
-  class TestErrorLog < self
+  class TestErrorLogLevel < self
     data("emergency" => "E",
          "alert"     => "A",
          "critical"  => "C",
